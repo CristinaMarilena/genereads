dbbd85bbf59cdfed184e6d026627c6955a59d454
cristinamarilena <cristinamarilena95@gmail.com>
2018-03-15 23:14:53 +0200

    Testing comment

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+
+        /*Comment for pull request*/
     }
 }
